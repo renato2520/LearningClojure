4bb8c7214768a84553a1c890235641295c05eb66
Renato A Cardenete <renatocardenete@outlook.com>
2017-06-11 20:59:19 -0300

    e^x

---
+++ Evaluating_e^x.clj
@@ -0,0 +1,49 @@
+(ns com.clojure.examples
+  (:use '[clojure.string :only (split triml)]))
+
+(let [n_t (read-line) n (Integer/parseInt n_t)]
+  (loop [a0 n]
+    (when (> a0 0)
+      (let [x_t (read-line) x (Float/parseFloat x_t)]
+        (println (format "%.4f" (e-x x 10))))
+      (recur (- a0 1)))))
+
+(defn e-x [x n]
+  (loop [acc 0 n n]
+    (if (< n 1)
+      acc
+      (recur (+ acc (/ (pow x (- n 1)) (factorial (- n 1)))) (dec n)))))
+
+(defn pow [x n]
+  (loop [acc 1 n n]
+    (if (zero? n)
+      acc
+      (recur (* x acc) (dec n)))))
+
+(defn factorial [n]
+  (loop [cnt n acc 1]
+    (if (zero? cnt)
+      acc
+      (recur (dec cnt) (* acc cnt)))))
+
+(println ((fn [x n]
+            (loop [acc 0 n n]
+              (if (< n 1)
+                acc
+                (recur (+ acc (/
+                               ((fn [x n]
+                                  (loop [acc 1 n n]
+                                    (if (zero? n)
+                                      acc
+                                      (recur (* x acc) (dec n)))))
+                                x
+                                (- n 1))
+                               ((fn [x]
+                                  (loop [cnt n acc 1]
+                                    (if (zero? cnt)
+                                      acc
+                                      (recur (dec cnt) (* acc cnt)))))
+                                (- n 1))))
+                       (dec n)))))
+          20.0
+          10))
